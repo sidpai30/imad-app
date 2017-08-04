e2c1ebcd9b6661f090d715fdf8f9390020536949
sidpai30 <30700485+sidpai30@users.noreply.github.com>
2017-08-05 00:50:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,3 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
-var img = document.getElementById('madi');
-img.onclick = function (){
-  img.style.marginLeft = "100px" ; 
-};
