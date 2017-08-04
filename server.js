b805bea3b0a44ded2c41e142396ce7891a6b4015
sidpai30 <30700485+sidpai30@users.noreply.github.com>
2017-08-05 00:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/article-two', function (req, res) {
 });
 
 app.get('/main.js', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'main.js.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/article-three', function (req, res) {
